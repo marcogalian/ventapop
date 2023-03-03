21bfd7554df6115d1e66bc0654b14203e77efe4c
Chechu GC <jgimceb@gmail.com>
2023-03-03 19:09:24 +0100

    cambiosFront030323

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -3,12 +3,12 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                    <div class="card-header">{{ __('Verifica tu correo electrónico') }}</div>
 
                     <div class="card-body">
                         @if (session('resent'))
                             <div class="alert alert-success" role="alert">
-                                {{ __('A fresh verification link has been sent to your email address.') }}
+                                {{ __('Hemos mandado un enlace de verificación a tu correo electrónico.') }}
                             </div>
                         @endif
 
