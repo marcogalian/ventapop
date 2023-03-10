e819d1c4713738aac2e45cf328ba7d7c569266eb
Marco Antonio Galian <marcogalian@hotmail.com>
2023-03-10 18:33:05 +0100

    footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,14 +1,14 @@
-<div class="container-fluid p-0">
-    <footer class="footer-container pt-4 my-4 border-top row">
-        <p class="col-sm-12 mb-0 text-muted text-light">© 2023 Ventapop, Marco & Chechu</p>
+<div class="container-fluid">
+    <footer class="footer-container pt-4 my-4 border-top row align-items-center">
+        <p class="col-4 col-md-4 col-sm-4 mb-0 text-muted text-light ps">© 2023 Ventapop, Marco & Chechu</p>
 
-        <a class="col-sm-12 navbar-brand logo text-light" href="{{ route('home')}}">VentaPop!</a>
+        <a class="col-4 col-md-4 col-sm-4 navbar-brand logo text-light text-center" href="{{ route('home')}}">VentaPop!</a>
 
-        <ul class="nav col-md-4 justify-content-end pe-5">
-            <li class="nav-item"><a href="{{ route('home')}}" class="nav-link px-2 text-muted">{{ __('Inicio')}}</a></li>
-            <li class="nav-item"><a href="{{ route('about')}}" class="nav-link px-2 text-muted">{{ __('Sobre los creadores')}}</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-            <li class="nav-item"><a href="{{ route('revisor.become') }}" class="nav-link px-2 text-muted">{{ __('Trabaja con nosotros')}}</a></li>
+        <ul class="nav col-4 col-md-4 col-sm-4 justify-content-end pe-4">
+            <li class="nav-item"><a href="{{ route('home')}}" class="nav-link px-2 text-light">{{ __('Inicio')}}</a></li>
+            <li class="nav-item"><a href="{{ route('about')}}" class="nav-link px-2 text-light">{{ __('Sobre los creadores')}}</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-light">FAQs</a></li>
+            <li class="nav-item"><a href="{{ route('revisor.become') }}" class="nav-link px-2 text-light">{{ __('Trabaja con nosotros')}}</a></li>
         </ul>
     </footer>
 </div>
