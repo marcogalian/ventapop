869c92f84ee7e1473cbe69475080562fbb37e58b
Marco Antonio Galian <marcogalian@hotmail.com>
2023-03-13 20:30:06 +0100

    Has eliminar imagenes con livewire

--- app/Http/Livewire/CreateAd.php
+++ app/Http/Livewire/CreateAd.php
@@ -3,16 +3,25 @@
 namespace App\Http\Livewire;
 use App\Models\Ad;
 use App\Models\Category;
+use App\Models\Image;
+use GuzzleHttp\Psr7\Request;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 
 class CreateAd extends Component
 {
+    use WithFileUploads;
+
+    
     public $title;
     public $body;
     public $price;
     public $category;
+    public $images = [];
+    public $temporary_images;
+    public $image;
 
     protected $rules = [
         'title'=>'required|min:4',
@@ -24,30 +33,66 @@ class CreateAd extends Component
     protected $messages = [
         'required'=>'Por favor rellena este campo, es obligatorio',
         'min'=>'Field :attribute should be longer than :min',
-        'numeric'=>'Field :attribute must be a number'
+        'numeric'=>'Field :attribute must be a number',
+        'temporary_images.required' => 'La imagen es obligatoria',
+        'temporary_images.*.image' => 'Los archivos tienen que ser imagenes',
+        'temporary_images.*.max' => 'La imagen supera los :max mb',
+        'images.image' => 'El archivo tiene que ser una imagen',
+        'images.max' =>'La imagen supera los :max mb',
     ];
 
     public function store()
     {
-        $validateField = $this->validate([
-            'title'=>'required|min:4',
-            'category'=>'required',
-            'price'=>'required|numeric',
-            'body'=>'required|min:8',
-        ]);
 
+        // datos validados
+        $validateData = $this->validate();
+
+        // busco la categoria
         $category = Category::find($this->category);
-        $ad = $category->ads()->create([
-            'title'=>$this->title,
-            'body'=>$this->body,
-            'price'=>$this->price,           
-        ]);
 
+        // Creo el anuncio a partir de la categoria usando la relacion y pasando los datos validados
+        $ad = $category->ads()->create($validateData);
+
+        // $ad = $category->ads()->create([
+        //     'title'=>$this->title,
+        //     'body'=>$this->body,
+        //     'price'=>$this->price,           
+        // ]);
+        
+        // Vuelvo a guardar el anuncio "pasando" por la relacion del usuario
         Auth::user()->ads()->save($ad); 
 
+        // Guardo cada imagen en el db y en el storage
+        if (count($this->images)) {
+            foreach($this->images as $image){
+                $ad->images()->create([
+                        'path'=>$image->store("images/$ad->id", 'public')
+                    ]);
+            }
+        }
+        
         session()->flash('message','Anuncio creado con éxito');
         $this->cleanForm();
     }
+
+    public function updatedTemporaryImages(){
+
+        if($this->validate([
+            'temporary_images.*'=>'image|max:2048'
+        ])){
+            foreach($this->temporary_images as $image){
+                $this->images[] = $image;
+            }
+        }
+    }
+
+    public function removeImage($key)
+    {
+        if(in_array($key, array_keys($this->images))){
+            unset($this->images[$key]);
+        }
+    }
+
 
     public function updated($propertyName)
     {
@@ -66,4 +111,6 @@ class CreateAd extends Component
     {
         return view('livewire.create-ad');
     }
+
+
 }
--- app/Models/Ad.php
+++ app/Models/Ad.php
@@ -36,4 +36,8 @@ class Ad extends Model
         return Ad::where('is_accepted', null)->count();
     }
     
+    public function images()
+    {
+        return $this->hasMany(Image::class);
+    }
 }
