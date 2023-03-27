622ebad0e9d261302d440b07f53d1a203d3f90d2
Marco Antonio Galian <marcogalian@hotmail.com>
2023-03-27 18:29:06 +0200

    about marco

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,12 +9,10 @@
             <div class="col-12 col-md-6 mt-2">                
                     <h2 class="text-center nombre_about mb-5">Marco Antonio Galian</h2>
                     <div class="d-flex justify-content-center">
-                       <img src="{{ asset('images/logo_ventapopclaro.png')}}" alt="Fotografía creador Marco"
-                        class="m-3 rounded fotografia_about"> 
+                       <img src="{{ asset('images/Foto curriculum.jpg')}}" alt="Fotografía creador Marco"
+                        class="m-3 fotografia_about"> 
                     </div>                    
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque similique adipisci quas facere
-                        delectus, fuga tenetur maxime eaque, consequuntur voluptatibus, consequatur quo? A est natus
-                        enim velit odio sequi sint.</p>
+                    <p>Marco Antonio Galián. Soy desarrollador junior fullStack, tengo conocimientos en Html, Css, Javascript, Bootstrap, Php y laravel, soy uno de los creadores de la web de articulos de segunda mano VentaPop!</p>
             </div>
 
             <div class="col-12 col-md-6 mt-2">                
